0e23fbe6a772aad63554190eb0c524e9203dc684
Florin Lipan <florinlipan@gmail.com>
2020-02-04 15:08:08 +0100

    Don't make SPACED_HOST mandatory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV.fetch("SPACED_HOST")
+  config.action_controller.asset_host = ENV["SPACED_HOST"]
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -65,7 +65,7 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("SPACED_HOST", "localhost"),
+    host: ENV.fetch["SPACED_HOST", "localhost"),
     port: ENV.fetch("SPACED_PORT", "3000").to_i
   }
 
