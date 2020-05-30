305ac40e60116f07b053348f37ea86cfa61b4e99
Florin Lipan <florinlipan@gmail.com>
2020-05-30 20:10:45 +0200

    System tests configuration

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  parallelize(workers: 1)
+  driven_by :selenium, using: ENV["HEADLESS"] ? :headless_firefox : :firefox, screen_size: [1400, 1400]
 end
