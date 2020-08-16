2795447e559d959ffa6c6728274a21da3dd4da78
Florin Lipan <florinlipan@gmail.com>
2020-08-16 17:58:10 +0200

    Some layout changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
+  before_action only: [:home, :about] { flash.discard }
+
   def home; end
+
+  def about; end
 
   def start
     redirect_to user_signed_in? ? user_root_path : new_user_registration_path
   end
-
-  def about; end
 end
