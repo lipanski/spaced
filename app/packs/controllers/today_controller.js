f03423a80e14d1c2bc47c38af4894068ac8b34cd
Florin Lipan <florinlipan@gmail.com>
2021-12-19 21:07:02 +0100

    Some more styling

--- app/packs/controllers/today_controller.js
+++ app/packs/controllers/today_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "answer" ]
+  static targets = [ "answer", "grade" ]
 
   showAnswer(event) {
     event.preventDefault()
     event.currentTarget.classList.add("hidden")
     this.answerTarget.classList.remove("hidden")
+    this.gradeTargets.forEach(function(grade) {
+      grade.removeAttribute("disabled")
+    })
   }
 }
 
