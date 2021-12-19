e77720bcead66410b7bda54057a4c64abec3534d
Florin Lipan <florinlipan@gmail.com>
2021-12-19 20:24:37 +0100

    Today & legend

--- app/packs/controllers/today_controller.js
+++ app/packs/controllers/today_controller.js
@@ -1,22 +1,12 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "answer", "gradeLink" ]
+  static targets = [ "answer" ]
 
   showAnswer(event) {
     event.preventDefault()
     event.currentTarget.classList.add("hidden")
     this.answerTarget.classList.remove("hidden")
-    this.gradeLinkTargets.forEach(function(gradeLink) {
-      gradeLink.removeAttribute("disabled")
-    })
-  }
-
-  rateAnswer(event) {
-    if (event.currentTarget.hasAttribute("disabled")) {
-      event.preventDefault()
-      event.stopImmediatePropagation()
-    }
   }
 }
 
