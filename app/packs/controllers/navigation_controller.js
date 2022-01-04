67a808ff7920bde7a398cc8d3e39db84605a869c
Florin Lipan <florinlipan@gmail.com>
2022-01-04 21:53:55 +0100

    Hamburger button

--- app/packs/controllers/navigation_controller.js
+++ app/packs/controllers/navigation_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "menu" ]
 
-  showMenu(event) {
-    event.currentTarget.classList.toggle("is-active")
-    this.menuTarget.classList.toggle("is-active")
+  toggle(event) {
+    event.preventDefault()
+    this.menuTarget.classList.toggle("hidden")
   }
 }
