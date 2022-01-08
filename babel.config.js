54a31f8b1f05a1dc2bb81cd8a5b994f7aeceec07
Florin Lipan <florinlipan@gmail.com>
2022-01-08 17:46:04 +0100

    Migrate to top level babel assumption setPublicClassFields

--- babel.config.js
+++ babel.config.js
@@ -36,17 +36,15 @@ module.exports = function(api) {
         }
       ]
     ].filter(Boolean),
+    assumptions: {
+      setPublicClassFields: true
+    },
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
+      '@babel/plugin-proposal-class-properties',
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
