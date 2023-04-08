b7cf4f474f5c400b0da4af05e55ac304fdc9c82b
Florin Lipan <florinlipan@gmail.com>
2023-04-08 15:42:25 +0200

    Drop Graphql plugins after upgrade

--- app/graphql/spaced_schema.rb
+++ app/graphql/spaced_schema.rb
@@ -4,8 +4,4 @@ class SpacedSchema < GraphQL::Schema
   default_max_page_size 50
 
   query(Types::QueryType)
-
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-  use GraphQL::Pagination::Connections
 end
