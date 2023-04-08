6de440e962850de3783a59dd14b1b56225ae8a31
Florin Lipan <florinlipan@gmail.com>
2023-04-08 15:46:26 +0200

    Enable public file server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,7 @@ Rails.application.configure do
     "Cache-Control" => "public, max-age=#{10.days.to_i}"
   }
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
